9b4c01dd1dd45c7baf53185ca1390904eb791802
Martina Lofqvist <2265215l@student.gla.ac.uk>
2018-02-07 21:40:15 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# Import the Category model 
+from rango.models import Category
+
 # Created a view called index
 # Return HttpResponse object, which takes a String parameter
 #representing the conent of the page we wish to send to the
 #client requesting the view 
 def index(request):
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
